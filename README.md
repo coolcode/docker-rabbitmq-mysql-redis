64de18837425a4ae2dbd383caf7b95bc2cafa217
coolcode <coolcode@live.com>
2023-12-23 17:16:39 +0800

    🥑

---
+++ README.md
@@ -0,0 +1,10 @@
+# What is it
+
+Using docker-compose to deploy rabbitMQ , Mysql and Redis in docker. 
+
+## How to use
+
+* run "./build-image.sh"
+
+* run "docker-compose up -d"
+
---
+++ build-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      docker                               $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker pull mysql:5.7
+
+docker pull rabbitmq:3-management
+
+docker pull redis:3.2.11
+
